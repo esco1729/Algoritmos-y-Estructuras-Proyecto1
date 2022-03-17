80b97f06eb4b8c0fe913addce98b7d37b73ad892
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-03-16 21:50:51 -0600

    Corrección

--- src/Functions.java
+++ src/Functions.java
@@ -3,9 +3,5 @@ public class Functions {
 	private Vista vista;
 	
 	
-	public void separate() {
-		for (int i = 0; i < wordsList[0].length(); i++) {
-		    wCharLetter = wChar[i];
-		    inputChar = input.next().charAt(0);
-	}
+
 }
