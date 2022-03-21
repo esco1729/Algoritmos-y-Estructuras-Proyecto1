d71df799af5ecf2b792365538b8fbaaf284c76fe
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-03-20 21:48:54 -0600

    cambios defun

--- src/Defun.java
+++ src/Defun.java
@@ -1,3 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Defun {
+
+    
+    private HashMap<String, Object> variables = new HashMap<>();
+    private List<Object> instrucciones;
+    private String s = "";
+    
+    
+    public Defun(String nombre, Object var, Object newIns){
+        List<String> variables = new ArrayList<>();
+        variables.add(var.toString());
+        List ins = (List) newIns;
+        this.s = nombre;
+        for (String item: variables){
+            this.variables.put(item, null);
+        }
+        this.instrucciones = instrucciones;
+    }
+    
+    public String getNombre(){
+        return this.s;
+    }
+    
+    
 
 }
