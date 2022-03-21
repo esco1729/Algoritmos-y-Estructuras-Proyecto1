296c192bb1c213a1be8f45104d70479421886c85
lourdessaa11 <saa21333@uvg.edu.gt>
2022-03-21 17:37:05 -0600

    Update Predicados.java

--- src/Predicados.java
+++ src/Predicados.java
@@ -1,17 +1,57 @@
 public class Predicados {
 
-	public boolean equal() {
-		return false;
+	
+	public boolean equal(Object a, Object b) {
+		return a.equals(b);
 		
 	}
 	
-	public boolean biggerThan(String expres) {
-		return false;
+	public boolean biggerThan(Object a, Object b) {
+		return (Double.parseDouble(a.toString()) > Double.parseDouble(b.toString()));
 			
 	}
 	
 	
-	public boolean smallerThan(String expres) {
+	public boolean smallerThan(Object a, Object b) {
+		return (Double.parseDouble(a.toString()) < Double.parseDouble(b.toString()));
+			
+	}
+
+	public boolean Atom(Object value){
+		
+		try { 
+			if((Integer)Integer.parseInt(value.toString()) instanceof Integer){
+				return true;
+			}
+		} 
+		catch (NumberFormatException e) { 
+			try {
+				if((Float)Float.parseFloat(value.toString()) instanceof Float){
+					return true;
+				}
+			} 
+			catch (NumberFormatException e2) { 
+				try {
+					if((Double)Double.parseDouble(value.toString()) instanceof Double){
+						return true;
+					}
+				} 
+				catch (Exception e3) { 
+					try{
+						String valor = value.toString();
+						if(valor instanceof String){
+							
+							return true;
+						}
+					}
+					catch(Exception e4){
+						return false;
+						}
+					}
+			}
+		}
 		return false;
 	}
+
 }
+
