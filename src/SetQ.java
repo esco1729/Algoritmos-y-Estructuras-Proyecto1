ca33880cfb18bec5078341fe40f86e48f1107054
lourdessaa11 <saa21333@uvg.edu.gt>
2022-03-22 21:58:01 -0600

    Update SetQ.java

--- src/SetQ.java
+++ src/SetQ.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class SetQ {
 
+    private static String quote = "";
+
+    /**
+     * instrucciones para analizar en el quote
+     * @param instrucciones
+     * @return string 
+     */
+    public static String quote(List instrucciones){
+
+        quote = Arrays.toString(instrucciones.toArray()).replace("[", "(").replace("]", ")").replaceAll(",","");
+        return quote;
+    }
+
+    /**
+     * definicion de la función set q
+     * @param instrucciones
+     */
+    public static void setq(List instrucciones){
+
+        for(int x = 0; x < (instrucciones.size()-1); x++){
+            if(instrucciones.get(x+2) instanceof List){
+              List sub = (List) instrucciones.get(x+2);
+              List sub2 = (List) sub.get(1);
+              System.out.println(instrucciones.get(x+1) + ": " + quote(sub2));
+            }else{
+              System.out.println(instrucciones.get(x+1) + ": "+instrucciones.get(x+2));
+            }
+            x++;
+        }
+    }    
 }
